6bd10e864c43e5c0054ca3f56e42400d76e0e7ea
safaesiraji <safae.siraji@edu.fh-joanneum.at>
2025-05-27 19:32:55 +0200

    Initial working version

---
+++ WebApp/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// PostgreSQL connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Auto-create database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+// Routes
+app.MapGet("/", async (AppDbContext db) =>
+    await db.Users.ToListAsync());
+
+app.MapPost("/add", async (AppDbContext db, User user) =>
+{
+    db.Users.Add(user);
+    await db.SaveChangesAsync();
+    return Results.Ok(user);
+});
+
+app.Run();
+
+// Models
+record User(int Id, string Name);
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<User> Users => Set<User>();
+}
